98f9087e92e74184b763d81614e82104b106871a
Carlos Vazquez <cvazquez@blackburnlabs.com>
2024-12-17 12:55:24 -0500

    Update edit form to use material ui

--- src/components/form.js
+++ src/components/form.js
@@ -1,145 +1,278 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { getSelectDateOptions } from "../helpers/form";
+import {
+    TextField,
+    MenuItem,
+    Select,
+    Button,
+    InputLabel,
+    FormControl,
+    Box,
+    Grid2,
+} from "@mui/material";
 
-
-export default function Form({
-    form, handleSubmit, handleTextUpdate,
-    handleCategoryInput, categoryOverlay, categoryNamesSelectedDisplay,
-    handleSeriesSelection, series, seriesSelectedDisplay,
-    handleEditorChange, handleSaveDraft, saveStatus,
-    saveDraftStatus, savedPostCategoriesStatus, savedPostFlickrSetStatus, savedPostSeriesStatus,
-    deletedPostSeriesStatus, deletedPostCategoriesStatus,
-}) {
-    return	<div className="edit">
+const Form = ({
+    form,
+    handleSubmit,
+    handleTextUpdate,
+    handleCategoryInput,
+    categoryOverlay,
+    categoryNamesSelectedDisplay,
+    handleSeriesSelection,
+    series,
+    seriesSelectedDisplay,
+    handleEditorChange,
+    handleSaveDraft,
+    saveStatus,
+    saveDraftStatus,
+    savedPostCategoriesStatus,
+    savedPostFlickrSetStatus,
+    savedPostSeriesStatus,
+    deletedPostSeriesStatus,
+    deletedPostCategoriesStatus,
+}) => (
+    <Box className="edit" p={3}>
         <form method="post" onSubmit={handleSubmit}>
-            <div className="meta-fields">
-                <input 	type="text"
-                    name="title"
-                    value={form.title}
-                    placeholder="Title"
-                    onChange={handleTextUpdate} />
+            <Grid2 container spacing={2}>
+                {/* Title Field */}
+                <Grid2 size={{ xs: 12 }}>
+                    <TextField
+                        fullWidth
+                        label="Title"
+                        name="title"
+                        value={form.title}
+                        onChange={handleTextUpdate}
+                        variant="outlined" />
+                </Grid2>
 
-                <input 	type="text"
-                    name="teaser"
-                    value={form.teaser}
-                    placeholder="Teaser"
-                    onChange={handleTextUpdate} />
+                {/* Teaser Field */}
+                <Grid2 size={{ xs: 12 }}>
+                    <TextField
+                        fullWidth
+                        label="Teaser"
+                        name="teaser"
+                        value={form.teaser}
+                        onChange={handleTextUpdate}
+                        variant="outlined" />
+                </Grid2>
 
-                <input 	type="text"
-                    name="metaDescription"
-                    value={form.metaDescription}
-                    placeholder="Meta Description"
-                    onChange={handleTextUpdate} />
+                {/* Meta Description Field */}
+                <Grid2 size={{ xs: 12 }}>
+                    <TextField
+                        fullWidth
+                        label="Meta Description"
+                        name="metaDescription"
+                        value={form.metaDescription}
+                        onChange={handleTextUpdate}
+                        variant="outlined" />
+                </Grid2>
 
-                <input 	type="text"
-                    name="metaKeyWords"
-                    value={form.metaKeyWords}
-                    placeholder="Meta Keywords"
-                    onChange={handleTextUpdate} />
+                {/* Meta Keywords Field */}
+                <Grid2 size={{ xs: 12 }}>
+                    <TextField
+                        fullWidth
+                        label="Meta Keywords"
+                        name="metaKeyWords"
+                        value={form.metaKeyWords}
+                        onChange={handleTextUpdate}
+                        variant="outlined" />
+                </Grid2>
 
-                <div>
-                    <select	name		= "publishYear"
-                        value		= {form.publishYear}
-                        onChange	= {handleTextUpdate}>
-                        {getSelectDateOptions.years()}
-                    </select>
-                    <select	name		= "publishMonth"
-                        value		= {form.publishMonth}
-                        onChange	= {handleTextUpdate}>
-                        {getSelectDateOptions.months()}
-                    </select>
-                    <select	name		= "publishDay"
-                        value		= {form.publishDay}
-                        onChange	= {handleTextUpdate}>
-                        {getSelectDateOptions.days()}
-                    </select>
+                {/* Publish Date Selectors */}
+                <Grid2 size={{ xs: 12 }} container spacing={2}>
+                    <Grid2 size={{ xs: 12, md: 8 }} container spacing={2} style={{ width: "auto" }}>
+                        <Grid2 size={{ xs: 4, md: 4 }} style={{ width: "auto" }}>
+                            <FormControl fullWidth>
+                                <InputLabel>Year</InputLabel>
+                                <Select
+                                    name="publishYear"
+                                    value={form.publishYear || Date().getFullYear()}
+                                    onChange={handleTextUpdate}
+                                    label="Year"
+                                >
+                                    {getSelectDateOptions.years().map((year) => (
+                                        <MenuItem key={year.props.value} value={year.props.value}>
+                                            {year}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid2>
+                        <Grid2 size={{ xs: 4, md: 2 }} style={{ width: "auto" }}>
+                            <FormControl fullWidth>
+                                <InputLabel>Month</InputLabel>
+                                <Select
+                                    name="publishMonth"
+                                    value={form.publishMonth}
+                                    onChange={handleTextUpdate}
+                                    label="Month"
+                                >
+                                    {getSelectDateOptions.months().map((month) => (
+                                        <MenuItem key={month.props.value} value={month.props.value}>
+                                            {month}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid2>
+                        <Grid2 size={{ xs: 4, md: 2 }} style={{ width: "auto" }}>
+                            <FormControl fullWidth>
+                                <InputLabel>Day</InputLabel>
+                                <Select
+                                    name="publishDay"
+                                    value={form.publishDay}
+                                    onChange={handleTextUpdate}
+                                    label="Day"
+                                >
+                                    {getSelectDateOptions.days().map((day) => (
+                                        <MenuItem key={day.props.value} value={day.props.value}>
+                                            {day}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 size={{ xs: 12, md: 4 }} container spacing={2}>
+                        <Grid2 size={{ xs: 4, md: 4 }}>
+                            <FormControl fullWidth>
+                                <InputLabel>Hour</InputLabel>
+                                <Select
+                                    name="publishHour"
+                                    value={form.publishHour}
+                                    onChange={handleTextUpdate}
+                                    label="Hour"
+                                >
+                                    {getSelectDateOptions.hours().map((hour) => (
+                                        <MenuItem key={hour.props.value} value={hour.props.value}>
+                                            {hour}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid2>
+                        <Grid2 size={{ xs: 2, md: 4 }}>
+                            <FormControl fullWidth>
+                                <InputLabel>Minute</InputLabel>
+                                <Select
+                                    name="publishMinute"
+                                    value={form.publishMinute}
+                                    onChange={handleTextUpdate}
+                                    label="Minute"
+                                >
+                                    {getSelectDateOptions.minutes().map((minute) => (
+                                        <MenuItem key={minute.props.value} value={minute.props.value}>
+                                            {minute}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
 
-							&nbsp;&nbsp;&nbsp;
-                    <select	name		= "publishHour"
-                        value		= {form.publishHour}
-                        onChange	= {handleTextUpdate}>
-                        {getSelectDateOptions.hours()}
-                    </select>
-                    <select	name		= "publishMinute"
-                        value		= {form.publishMinute}
-                        onChange	= {handleTextUpdate}>
-                        {getSelectDateOptions.minutes()}
-                    </select>
-                </div>
+                {/* Categories Input */}
+                <Grid2 size={{ xs: 12 }}>
+                    <TextField
+                        fullWidth
+                        autoComplete="off"
+                        label="Start Typing a Category"
+                        name="categories"
+                        value={form.categoryName}
+                        onChange={handleCategoryInput}
+                        variant="outlined" />
+                    <Box>{categoryOverlay}</Box>
+                    <Box>{categoryNamesSelectedDisplay}</Box>
+                </Grid2>
 
-                <input	type		= "text"
-                    autoComplete = "off"
-                    name		= "categories"
-                    placeholder	= "Start Typing a Category"
-                    value		= {form.categoryName}
-                    onChange	= {handleCategoryInput} />
-                <div>{categoryOverlay}</div>
-                <ul className="category-names-selected">{categoryNamesSelectedDisplay}</ul>
+                {/* Series Selector */}
+                <Grid2 size={{ xs: 12 }}>
+                    <FormControl fullWidth>
+                        <InputLabel>Add to a Series</InputLabel>
+                        <Select
+                            name="series"
+                            value={form.seriesId || ''}
+                            onChange={handleSeriesSelection}
+                            label="Add to a Series"
+                        >
+                            <MenuItem value="">-- Add to a Series --</MenuItem>
+                            {series.map((item) => (
+                                <MenuItem key={item.id} value={item.id}>
+                                    {item.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Box>{seriesSelectedDisplay}</Box>
+                </Grid2>
 
-                <select name		= "series"
-                    value		= {form.seriesId}
-                    onChange	= {handleSeriesSelection}>
-                    <option value="">-- Add to a Series --</option>
-                    {series.map(series => (
-                        <option key		= {series.id}
-                            value	= {series.id}>
-                            {series.name}
-                        </option>
-                    ))}
-                </select>
-                <ul className="series-names-selected">
-                    {seriesSelectedDisplay}
-                </ul>
+                {/* Flickr Set Selector */}
+                <Grid2 size={{ xs: 12 }}>
+                    <FormControl fullWidth>
+                        <InputLabel>Attach To A Flickr Set</InputLabel>
+                        <Select
+                            name="flickrSetId"
+                            value={form.flickrSetId || ''}
+                            onChange={handleTextUpdate}
+                            label="Attach To A Flickr Set"
+                        >
+                            <MenuItem value="">-- Attach To A Flickr Set --</MenuItem>
+                            {form.flickrSets.map((set) => (
+                                <MenuItem key={set.id} value={set.id}>
+                                    {set.title}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid2>
 
-                <select name		= "flickrSetId"
-                    value		= {form.flickrSetId}
-                    onChange	= {handleTextUpdate}>
-                    <option value="">-- Attach To A Flickr Set --</option>
-                    {form.flickrSets.map(flickrSet => (
-                        <option key		= {flickrSet.id}
-                            value	= {flickrSet.id}>
-                            {flickrSet.title}
-                        </option>
-                    ))}
-                </select>
-            </div>
+                {/* TinyMCE Editor */}
+                <Grid2 size={{ xs: 12 }}>
+                    <Box className="editor">
+                        <Editor
+                            initialValue={form.content}
+                            apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
+                            init={{
+                                menubar: false,
+                                plugins: [
+                                    "save advlist autolink lists link image charmap print preview anchor",
+                                    "searchreplace visualblocks code fullscreen",
+                                    "insertdatetime media table paste code help wordcount",
+                                ],
+                                toolbar: "save undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code | help",
+                                save_onsavecallback: () => { },
+                            }}
+                            onEditorChange={handleEditorChange}
+                            onSaveContent={handleSaveDraft} />
+                    </Box>
+                </Grid2>
 
-            {saveDraftStatus}
+                {/* Submit Button */}
+                <Grid2 size={{ xs: 12 }}>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        data-testid="Save"
+                    >
+                        Save
+                    </Button>
+                </Grid2>
 
-            <div className="editor">
-                <Editor
-                    initialValue	= {form.content}
-                    apiKey			= {process.env.REACT_APP_TINYMCE_API_KEY}
-                    init			= {{
-                        menubar	: false,
-                        plugins	: [
-                            "save advlist autolink lists link image charmap print preview anchor",
-                            "searchreplace visualblocks code fullscreen",
-                            "insertdatetime media table paste code help wordcount"
-                        ],
-                        toolbar	:
-									`save undo redo | formatselect | bold italic backcolor |
-									alignleft aligncenter alignright alignjustify |
-									bullist numlist outdent indent | removeformat | code | help`,
-                        save_onsavecallback: function () {}
-                    }}
-                    onEditorChange	= {handleEditorChange}
-                    onSaveContent	= {handleSaveDraft}
-                />
-            </div>
-
-            {saveStatus}
-            {deletedPostCategoriesStatus}
-            {savedPostCategoriesStatus}
-            {savedPostFlickrSetStatus}
-            {savedPostSeriesStatus}
-            {deletedPostSeriesStatus}
-
-            <input	type		= "submit"
-                name		= "submitPost"
-                value		= "Save"
-                data-testid	= "Save"  />
+                {/* Status Messages */}
+                <Grid2 size={{ xs: 12 }}>
+                    {saveDraftStatus}
+                    {saveStatus}
+                    {deletedPostCategoriesStatus}
+                    {savedPostCategoriesStatus}
+                    {savedPostFlickrSetStatus}
+                    {savedPostSeriesStatus}
+                    {deletedPostSeriesStatus}
+                </Grid2>
+            </Grid2>
         </form>
-    </div>;
-}
+    </Box>
+);
+
+export default Form;
